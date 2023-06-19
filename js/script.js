2188371a249a1e4e2922d1f6300697adb18364e2
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-06-19 17:46:00 +0200

    add step list

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-console.log("hello world")
+/*
+Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+
+RAGIONAMENTO BASE
+1. creare un array che conterrà la lista della spesa 
+2. creare un ciclo while che percorre l'array
+  - per ogni elemento dell'array creare un elemento html 
+    - inserire il dato corrente della lista nell'elemento creato
+  -inserire l'elemento html nell contenitore nell'html 
+
+*/
